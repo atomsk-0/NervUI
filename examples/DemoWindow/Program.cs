97eece8577ba95d597c20d150c58c706cffbac52
byte-0x74 <wexberkyhd@gmail.com>
2023-01-05 22:02:07 +0200

    Add comments to example

--- examples/DemoWindow/Program.cs
+++ examples/DemoWindow/Program.cs
@@ -4,6 +4,7 @@ using NervUI;
 
 namespace DemoWindow;
 
+//Layers are to help keep the imgui code cleaner so you can create new class for each imgui part you want.
 public class DemoLayer : Layer
 {
     public override unsafe void OnUIRender()
@@ -35,19 +36,24 @@ internal static class Program
     private static bool g_applicationRunning = true;
     private static void Main()
     {
+        //Create main loop for the application
         while (g_applicationRunning)
         {
+            //Options for App
             var options = new ApplicationOptions()
             {
                 Title = "Demo Window",
                 Size = new(1280, 720)
             };
             
+            //Create new instance of the application
             _application = Application.CreateApplication(options);
             
+            //Push DemoLayer and DemoLayer2 for the renderer
             _application.PushLayer<DemoLayer>();
             _application.PushLayer<DemoLayer2>();
             
+            //Run application
             _application.Run();
             
             g_applicationRunning = false;
