4191250ccf501182070a2f2d66a6c9b9f4e631e5
byte-0x74 <wexberkyhd@gmail.com>
2023-01-09 07:56:05 +0000

    Fixed low fps issue in NervUI Debug window caused by InputTextMultiline with big text buffer size

--- src/NervUI/Modules/NervUIDebug.cs
+++ src/NervUI/Modules/NervUIDebug.cs
@@ -15,13 +15,13 @@ public static unsafe class NervUIDebug
         ImGui.Begin("NervUI Debug", show, ImGuiWindowFlags.NoResize);
         {
             ImGui.Text($"Application average {1000f / io->Framerate} ms/frame ({io->Framerate} FPS)");
-            ImGui.Text($"Dear ImGui {ImGui.IMGUI_VERSION}");
+           ImGui.Text($"Dear ImGui {ImGui.IMGUI_VERSION}");
             ImGui.Text($"OpenGL 3.0");
             ImGui.Text("NervUI by byte-0x74");
             ImGui.Separator();
             ImGui.Text("NervUI Logs");
             ImGui.PushStyleColor(ImGuiCol.FrameBg, Util.Vec_Color(44, 44, 44));
-            ImGuiManaged.InputTextMultiLine("##nvuidbgl", ref DebugLogText, 9999999,
+            ImGuiManaged.InputTextMultiLine("##nvuidbgl", ref DebugLogText, 500,
                 new OpenTK.Mathematics.Vector2(720, 270),
                 ImGuiInputTextFlags.ReadOnly);
             ImGui.PopStyleColor();
